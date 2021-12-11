1777b7558acacba48e83ad75abe6d609985edd10
honeysoniverse <95574885+honeysoniverse@users.noreply.github.com>
2021-12-11 17:05:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+document.getElementById('btn').addEventListener('click',function(e){
+    e.preventDefault();
+    
+    getWeather();
+});
+
+
+//Initialise Weather object
+const weather = new Weather(input);
+
+//Initialise UI Object
+const ui = new UI;
+
+//Get weather on dom load
+
+
+
+//document.getElementById('form-control').addEventListener('click',getWeather)
+
+function getWeather(){
+    const input = document.getElementById('input').value;
+    const weather = new Weather(input);
+    
+weather.getWeather()
+
+.then(results => {ui.paint(results)})
+
+.then(toggletran())
+
+.then(togglevis())
+
+
+.catch(err => console.log(err));
+
+}
+
+
